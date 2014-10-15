b55e3328038a25c7864308be0371b568041a9ef7
clara_000 <clara_000@ClaraPinelli>
2014-10-14 23:30:30 -0200

    Arreglo error de paquete

--- Sigamas-war/src/java/uy/edu/ort/sigamas/sigamasweb/usuario/BeanUsuario.java
+++ Sigamas-war/src/java/uy/edu/ort/sigamas/sigamasweb/usuario/BeanUsuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package uy.edu.ort.sigamas.sigamasweb.login;
+package uy.edu.ort.sigamas.sigamasweb.usuario;
 
 import java.util.Date;
 import java.util.List;
