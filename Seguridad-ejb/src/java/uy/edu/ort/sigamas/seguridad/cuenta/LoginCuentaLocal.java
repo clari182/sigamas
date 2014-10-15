69de7c81ccb850ae22b5ee04d342732691640747
clara_000 <clara_000@ClaraPinelli>
2014-10-14 23:19:41 -0200

    Creacion BeanCuenta

---
+++ Seguridad-ejb/src/java/uy/edu/ort/sigamas/seguridad/cuenta/LoginCuentaLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.seguridad.cuenta;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Local
+public interface LoginCuentaLocal {
+    
+}
