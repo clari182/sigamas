99dcf1e992715b19a943b514367d9d5441629f9a
Clara Youdale <clarayoudale@outlook.com>
2014-10-21 13:18:11 -0200

    Creacion Exception por nombre de cuenta existente

--- Seguridad-ejb/src/java/uy/edu/ort/sigamas/seguridad/cuenta/utils/UtilCuenta.java
+++ Seguridad-ejb/src/java/uy/edu/ort/sigamas/seguridad/cuenta/utils/UtilCuenta.java
@@ -8,6 +8,7 @@ package uy.edu.ort.sigamas.seguridad.cuenta.utils;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import uy.edu.ort.sigamas.seguridad.cuenta.excepciones.CuentaExistenteException;
 import uy.edu.ort.sigamas.seguridad.entidades.Cuenta;
 
 /**
@@ -15,8 +16,8 @@ import uy.edu.ort.sigamas.seguridad.entidades.Cuenta;
  * @author Pikachuss
  */
 public class UtilCuenta {
-    public static Cuenta crearCuenta(EntityManager em, String nombre, String empresa, String rut){
-        
+    public static Cuenta crearCuenta(EntityManager em, String nombre, String empresa, String rut) throws CuentaExistenteException{
+        UtilCuenta.verificarNombreCuenta(em, nombre);
         Cuenta cuenta = new Cuenta(null, nombre, empresa);
         cuenta.setRut(rut);
         em.persist(cuenta);
@@ -26,5 +27,12 @@ public class UtilCuenta {
     public static List<Cuenta> obtenerCuentas(EntityManager em){
         Query cuentas = em.createNamedQuery("Cuenta.findAll");
         return cuentas.getResultList();
+    }
+
+    public static void verificarNombreCuenta(EntityManager em, String nombre) throws CuentaExistenteException{
+       Query q = em.createNamedQuery("Cuenta.findByNombre").setParameter("nombre", nombre);
+       if (!q.getResultList().isEmpty()){
+           throw new CuentaExistenteException();
+       }
     }
 }
