1dca115fd7d81681bd282030455f9a0a10e9cc1a
Clara Youdale <clarayoudale@outlook.com>
2014-10-29 12:03:22 -0200

    Creacion ParcelaBean y UtilParcela

---
+++ Seguridad-ejb/src/java/uy/edu/ort/sigamas/seguridad/parcela/ParcelaBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.seguridad.parcela;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Stateless
+public class ParcelaBean implements ParcelaBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ Sigamas-war/src/java/uy/edu/ort/sigamas/sigamasweb/parcela/BeanParcela.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.sigamasweb.parcela;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Named(value = "beanPredio")
+@ViewScoped
+public class BeanParcela {
+
+    /**
+     * Creates a new instance of BeanPredio
+     */
+    public BeanParcela() {
+    }
+    
+}
