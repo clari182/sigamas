6606217dbd343aaf851e8495cc832124736df1ee
Clara Youdale <clarayoudale@outlook.com>
2014-11-04 11:23:12 -0200

    Principio de la funcion de login

--- SigamasBackofficeWeb/src/java/uy/edu/ort/sigamas/sigamasbackofficeweb/login/BeanLogin.java
+++ SigamasBackofficeWeb/src/java/uy/edu/ort/sigamas/sigamasbackofficeweb/login/BeanLogin.java
@@ -7,6 +7,7 @@ package uy.edu.ort.sigamas.sigamasbackofficeweb.login;
 
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import uy.edu.ort.sigamas.seguridad.login.LoginBeanLocal;
 
 /**
  *
@@ -16,10 +17,59 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class BeanLogin {
 
+    private String nombreUsuario;
+    private String claveUsuario;
+    private LoginBeanLocal loginBeanLocal;
     /**
      * Creates a new instance of BeanLogin
      */
     public BeanLogin() {
     }
-    
+
+    /**
+     * @return the nombreUsuario
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * @param nombreUsuario the nombreUsuario to set
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    /**
+     * @return the claveUsuario
+     */
+    public String getClaveUsuario() {
+        return claveUsuario;
+    }
+
+    /**
+     * @param claveUsuario the claveUsuario to set
+     */
+    public void setClaveUsuario(String claveUsuario) {
+        this.claveUsuario = claveUsuario;
+    }
+
+    /**
+     * @return the loginBeanLocal
+     */
+    public LoginBeanLocal getLoginBeanLocal() {
+        return loginBeanLocal;
+    }
+
+    /**
+     * @param loginBeanLocal the loginBeanLocal to set
+     */
+    public void setLoginBeanLocal(LoginBeanLocal loginBeanLocal) {
+        this.loginBeanLocal = loginBeanLocal;
+    }
+        
+    public void ingresar(){
+        
+    }
+
 }
