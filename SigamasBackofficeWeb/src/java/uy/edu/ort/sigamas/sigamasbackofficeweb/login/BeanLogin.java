aa628fcfbd2a8068e3673042932098c74b3a3867
Clara Youdale <clarayoudale@outlook.com>
2014-11-04 11:17:46 -0200

    Creación branchBackoffice con las librerias necesarias

---
+++ SigamasBackofficeWeb/src/java/uy/edu/ort/sigamas/sigamasbackofficeweb/login/BeanLogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.sigamasbackofficeweb.login;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Named(value = "beanLogin")
+@Dependent
+public class BeanLogin {
+
+    /**
+     * Creates a new instance of BeanLogin
+     */
+    public BeanLogin() {
+    }
+    
+}
---
+++ SigamasBackofficeWeb/src/java/uy/edu/ort/sigamas/sigamasbackofficeweb/superusuario/BeanSuperUsuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.sigamasbackofficeweb.superusuario;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Named(value = "beanSuperUsuario")
+@Dependent
+public class BeanSuperUsuario {
+
+    /**
+     * Creates a new instance of BeanSuperUsuario
+     */
+    public BeanSuperUsuario() {
+    }
+    
+}
