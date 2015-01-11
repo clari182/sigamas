595eeeea4dc969c35206158063d5f0708399a006
Clara Youdale <clarayoudale@outlook.com>
2015-01-11 18:59:03 -0200

    Nuevo modulo sms

---
+++ sms-ejb/src/java/uy/edu/ort/sigamas/sms/sms/SmsBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.sigamas.sms.sms;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Pikachuss
+ */
+@Stateless
+public class SmsBean implements SmsBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
